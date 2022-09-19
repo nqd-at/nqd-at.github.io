a76fb1ac485060104350a4905c1e8df219306229
Joey <nguyenquocdat.vn@gmail.com>
2022-09-19 21:18:42 +0700

    initial page

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,75 @@
-import reactLogo from './assets/react.svg'
-
 function App() {
   return (
-    <div className='bg-slate-100'>
-      <div>
-        <a href='https://vitejs.dev' target='_blank'>
-          <img src='/vite.svg' className='logo' alt='Vite logo' />
-        </a>
-        <a href='https://reactjs.org' target='_blank'>
-          <img src={reactLogo} className='logo react' alt='React logo' />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className='card'>
-        <p>Hello World!</p>
-      </div>
-      <p className='read-the-docs'>
-        Click on the Vite and React logos to learn more
-      </p>
+    <div className='p-24 space-y-16 max-w-3xl'>
+      <h1 className='text-8xl font-extrabold leading-[.6] tracking-tighter'>
+        &lt;nqdat /&gt;
+        <br />
+        <span className='text-white text-6xl lowercase word-spacing-1/2'>Development team</span>
+      </h1>
+      <section>
+        <h2>What we do</h2>
+        <ul>
+          <li>
+            <p>System Design</p>
+          </li>
+          <li>
+            <p>Project Management</p>
+          </li>
+          <li>
+            <p>Web Development</p>
+          </li>
+          <li>
+            <p>Continuous Integration &amp; Deployment</p>
+          </li>
+        </ul>
+      </section>
+      <section>
+        <h2>Our team</h2>
+        <ul className='space-y-4'>
+          <li>
+            <h3>Joey Nguyen</h3>
+            <p className='text-slate-500 text-sm'>
+              Technical Architect/Team lead
+            </p>
+            <p>
+              <a href='https://'>LinkedIn</a>
+            </p>
+          </li>
+          <li>
+            <h3>Nhan Huynh</h3>
+            <p className='text-slate-500 text-sm'>Senior Developer</p>
+            <p>
+              <a href='https://'>LinkedIn</a>
+            </p>
+          </li>
+        </ul>
+      </section>
+      <section>
+        <h2>Experiences</h2>
+        <ul className='space-y-4'>
+          <li>
+            <h3>Deloitte Levvia</h3>
+            <p className='text-slate-500 text-sm'>Deloitte TTL's Very Small Auditing tool</p>
+            <div className="space-x-4">
+              <a href='https://'>Info</a>
+              <a href='https://'>Visit</a>
+            </div>
+          </li>
+          <li>
+            <h3>Avitrip</h3>
+            <p className='text-slate-500 text-sm'>Online booking service for Travel Agencies</p>
+            <p>
+              <a href='https://'>LinkedIn</a>
+            </p>
+          </li>
+        </ul>
+      </section>
+      <section>
+        <h2>Give a try</h2>
+        <div>
+          <a href='https://'>Email us</a>
+        </div>
+      </section>
     </div>
   )
 }
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,30 @@
 /** @type {import('tailwindcss').Config} */
+
+const tw = require('tailwindcss/defaultConfig')
+
 module.exports = {
   content: ['./index.html', './src/**/*.{ts,tsx}'],
   theme: {
+    fontFamily: {
+      sans: [
+        'Inter var',
+        'Inter',
+        'ui-sans-serif',
+        'system-ui',
+        '-apple-system',
+        'BlinkMacSystemFont',
+        '"Segoe UI"',
+        'Roboto',
+        '"Helvetica Neue"',
+        'Arial',
+        '"Noto Sans"',
+        'sans-serif',
+        '"Apple Color Emoji"',
+        '"Segoe UI Emoji"',
+        '"Segoe UI Symbol"',
+        '"Noto Color Emoji"',
+      ],
+    },
     extend: {},
   },
   plugins: [require('@tailwindcss/typography')],
